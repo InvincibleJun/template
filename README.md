ae05bba1d1e739e23f26c7b4d12ebcb794b47acf
luquechen <luquechen@tencent.com>
2024-09-27 15:33:40 +0800

    feat: 提交

---
+++ README.md
@@ -0,0 +1,12 @@
+# env
+
+node v20.9.0
+pnpm 9.5.0
+
+# pnpm
+pnpm install
+
+pnpm run build
+
+node .output/server/index.mjs
+
---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Button>test</Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Button } from 'ant-design-vue';
+
+</script>
